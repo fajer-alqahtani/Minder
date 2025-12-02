3dd0f0769d6959d7ff59c4e96dc5dabce98f49bf
Sarah <Sarahalsarah77@gmail.com>
2025-12-02 09:36:10 +0300

    edited main page, removed settings view and pop up, edited splash screen destination

--- Minder/View/MainPage.swift
+++ Minder/View/MainPage.swift
@@ -51,20 +51,6 @@ struct MainPage: View {
                                 .padding(.top, 50)
                             }
                         
-                        // Settings button (top-right)
-                            .overlay(alignment: .topTrailing) {
-                                Button {
-                                    viewModel.didTapSettings()
-                                } label: {
-                                    Image(systemName: "line.3.horizontal.decrease.circle")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundColor(Color.ourGrey)
-                                        .frame(width: 28, height: 28)
-                                }
-                                .padding(.trailing, 24)
-                                .padding(.top, 80)
-                            }
                     }
                     .padding(.top, -70)
                     
@@ -72,126 +58,98 @@ struct MainPage: View {
                 }
                 .padding()
                 
-                // POPUP OVERLAY
-                if viewModel.isShowingSettings {
-                    // dimmed background
-                    Color.black.opacity(0.4)
-                        .ignoresSafeArea()
-                        .onTapGesture {
-                            viewModel.closeSettings()
-                        }
-                    
-                    // popup card
-                    VStack(spacing: 16) {
-                        Text("Settings")
-                            .font(.headline)
-                        
-                        Button("Close") {
-                            viewModel.closeSettings()
-                        }
-                        .padding(.top, 8)
-                    }
-                    .padding(24)
-                    .background(
-                        RoundedRectangle(cornerRadius: 24)
-                            .fill(Color(.systemBackground))
-                    )
-                    .frame(maxWidth: 320)
-                    .shadow(radius: 20)
-                }
-                
                 VStack(spacing: 12) {
                     NavigationLink(
-                                            destination:
-                                                MedicationView()
-                                        ) {
-                                            HStack(spacing: 12) {
-                                                Image(systemName: "pills.fill")
-                                                    .font(.system(size: 20))
+                        destination:
+                            MedicationView()
+                    ) {
+                        HStack(spacing: 12) {
+                            Image(systemName: "pills.fill")
+                                .font(.system(size: 20))
 
-                                                Text("Medications")
-                                                    .font(.system(size: 16, weight: .semibold))
-                                                    .foregroundColor(.ourDarkGrey)
+                            Text("Medications")
+                                .font(.system(size: 16, weight: .semibold))
+                                .foregroundColor(.ourDarkGrey)
 
-                                                Spacer()
+                            Spacer()
 
-                                                Image(systemName: "chevron.right")
-                                                    .font(.system(size: 14))
-                                                    .foregroundColor(.secondary)
-                                            }
-                                            .padding(.vertical, 26)
-                                            .padding(.horizontal, 20)
-                                            .background(
-                                                RoundedRectangle(cornerRadius: 14)
-                                                    .fill(Color(.systemGray6))
-                                            )
-                                            .frame(width: 290)  // control rectangle width
-                                        }
-                                        .buttonStyle(.plain)
-                                        .padding(.top, 240)
-                    
-                    NavigationLink(
-                                           destination:
-                                               Text("Meals Page") //  Just a placeholder, I need to write the view name example: Meals View()
-                                       ) {
-                                           HStack(spacing: 12) {
-                                               Image(systemName: "fork.knife")
-                                                   .font(.system(size: 20))
-
-                                               Text("Meals")
-                                                   .font(.system(size: 16, weight: .semibold))
-                                                   .foregroundColor(.ourDarkGrey)
-
-                                               Spacer()
-
-                                               Image(systemName: "chevron.right")
-                                                   .font(.system(size: 14))
-                                                   .foregroundColor(.secondary)
-                                           }
-                                           .padding(.vertical, 26)
-                                           .padding(.horizontal, 20)
-                                           .background(
-                                               RoundedRectangle(cornerRadius: 14)
-                                                   .fill(Color(.systemGray6))
-                                           )
-                                           .frame(width: 290)
-                                       }
-                                       .buttonStyle(.plain)
-                                       .padding(.top, 30)
-                    
-                    
-                    NavigationLink(
-                                           destination:
-                                               EmotionalStatusView()
-                                       ) {
-                                           HStack(spacing: 12) {
-                                               Image(systemName: "heart.text.square.fill")
-                                                   .font(.system(size: 20))
-
-                                               Text("Emotional Status")
-                                                   .font(.system(size: 16, weight: .semibold))
-                                                   .foregroundColor(.ourDarkGrey)
-
-                                               Spacer()
-
-                                               Image(systemName: "chevron.right")
-                                                   .font(.system(size: 14))
-                                                   .foregroundColor(.secondary)
-                                           }
-                                           .padding(.vertical, 26)
-                                           .padding(.horizontal, 20)
-                                           .background(
-                                               RoundedRectangle(cornerRadius: 14)
-                                                   .fill(Color(.systemGray6))
-                                           )
-                                           .frame(width: 290)
-                                       }
-                                       .buttonStyle(.plain)
-                                       .padding(.top, 30)
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 14))
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(.vertical, 26)
+                        .padding(.horizontal, 20)
+                        .background(
+                            RoundedRectangle(cornerRadius: 14)
+                                .fill(Color(.systemGray6))
+                        )
+                        .frame(width: 290)  // control rectangle width
+                    }
+                    .buttonStyle(.plain)
+                    .padding(.top, 240)
                     
                     NavigationLink(
                         destination:
-                            Text("Summary Trial Page") //  Just a placeholder, I need to write the view name example: Emotional View()
+                            Text("Meals Page") //  Just a placeholder, replace with MealsView() when available
+                    ) {
+                        HStack(spacing: 12) {
+                            Image(systemName: "fork.knife")
+                                .font(.system(size: 20))
+
+                            Text("Meals")
+                                .font(.system(size: 16, weight: .semibold))
+                                .foregroundColor(.ourDarkGrey)
+
+                            Spacer()
+
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 14))
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(.vertical, 26)
+                        .padding(.horizontal, 20)
+                        .background(
+                            RoundedRectangle(cornerRadius: 14)
+                                .fill(Color(.systemGray6))
+                        )
+                        .frame(width: 290)
+                    }
+                    .buttonStyle(.plain)
+                    .padding(.top, 30)
+                    
+                    
+                    NavigationLink(
+                        destination:
+                            EmotionalStatusView()
+                    ) {
+                        HStack(spacing: 12) {
+                            Image(systemName: "heart.text.square.fill")
+                                .font(.system(size: 20))
+
+                            Text("Emotional Status")
+                                .font(.system(size: 16, weight: .semibold))
+                                .foregroundColor(.ourDarkGrey)
+
+                            Spacer()
+
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 14))
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(.vertical, 26)
+                        .padding(.horizontal, 20)
+                        .background(
+                            RoundedRectangle(cornerRadius: 14)
+                                .fill(Color(.systemGray6))
+                        )
+                        .frame(width: 290)
+                    }
+                    .buttonStyle(.plain)
+                    .padding(.top, 30)
+                    
+                    NavigationLink(
+                        destination:
+                            Text("Summary Trial Page") //  Just a placeholder, replace with SummaryView() when available
                     ) {
                         Text("Summary")
                             .font(Font.title)
